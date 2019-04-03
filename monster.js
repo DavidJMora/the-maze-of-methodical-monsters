83e0b29d113a31634cccca44e7b9b1b65b46f638
David Mora <David.mora@codeimmersives.com>
2019-04-03 17:43:36 -0400

    created properties for both monster and player pages. created method within player object to make changes to player health and damage to reflect on HTML.

--- monster.js
+++ monster.js
@@ -1,3 +1,6 @@
 const monster = {
-
+    hitPoints: 30,
+    lastDamageTaken: 0,
+    isAlive: true,
+    
 };
--- player.js
+++ player.js
@@ -1,3 +1,17 @@
 const player = {
+    isAlive: true,
+    fight: function damageReceived() {
+        const min = Math.min(1);
+        const randomDamage = Math.ceil(Math.random() * (min + 4));
+        this.lastDamageTaken = randomDamage;
+        const newHitPoints = this.hitPoints - randomDamage;
+        this.hitPoints = newHitPoints;
+        if(this.hitPoints === 0) {
+            this.isAlive = false;
+        }
+    },
+    lastDamageTaken: 0,
+    hitPoints: 100,
+    level: 1,
 
 }
