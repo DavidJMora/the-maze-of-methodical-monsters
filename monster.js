de6a6760240d0988b69d37702a799e291a37e5a7
David Mora <David.mora@codeimmersives.com>
2019-04-03 21:03:20 -0400

    created method for monster to take and reflect damage properly on HTML. as well as a method within players to randomize damage and sent to monsters.

--- monster.js
+++ monster.js
@@ -7,4 +7,9 @@ const monster = {
         const randomDamage = Math.ceil(Math.random() * (min + 4));
         return randomDamage + 2;
     },
+    takeDamage: function(rawDamage) {
+        this.lastDamageTaken = rawDamage;
+        const newHitPoints = this.hitPoints - rawDamage;
+        this.hitPoints = newHitPoints;
+    }
 };
--- player.js
+++ player.js
@@ -8,11 +8,12 @@ const player = {
         if(this.hitPoints <= 0) {
             this.isAlive = false;
         }
+        enemy.takeDamage(this.calculateRawDamage());
     },
     calculateRawDamage: function() {
         const min = Math.min(1);
-        const randomDamage = Math.ceil(Math.random() * (min + 4));
-        return randomDamage;
+        const rawDamage = Math.ceil(Math.random() * (min + 4));
+        return rawDamage;
     },
     lastDamageTaken: 0,
     hitPoints: 100,
