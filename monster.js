c23280e27b3823b96c85aad3c926b4bc7aa818a5
David Mora <David.mora@codeimmersives.com>
2019-04-04 00:13:43 -0400

    added comments

--- monster.js
+++ monster.js
@@ -5,29 +5,49 @@ const monster = {
     lastDamageTaken: 0,
     isAlive: true,
     baseDamage: 2,
-
+// Everything above this comment are the base stats for the monsters
+// 
+// Method designed to calculate a damage output to be subtracted from player's hitpoints
     calculateRawDamage: function() {
+        // Sets minimum value to one
         const min = Math.min(1);
+        // Randomizes a number between the values of one and six and rounds down to the nearest integer
         const randomDamage = Math.floor(Math.random() * (min + 5));
+        // returns the value of randomDamage and adds it to the current value of baseDamage
         return randomDamage + this.baseDamage;
     },
+// This method takes the information sent to it from player and applies it as damage accordingly
     takeDamage: function(rawDamage) {
+        // Variable newHitPoints takes monster's current hitpoints and subtracts damage done by player
         const newHitPoints = this.hitPoints - rawDamage;
+        // hitPoints then sets the value of newHitPoints as the monster's current hitpoint value
         this.hitPoints = newHitPoints;
+        // Replaces old value with current value of damage taken
         this.lastDamageTaken = rawDamage;
+        // When monster's hitpoints falls to zero or below change isALive to false
         if(this.hitPoints <= 0) {
             this.isAlive = false;
         }
     },
+// This method takes effect once the player has reduced the monster's hitpoints to zero or less
     respawn: function() {
+        // Pulls current value in minHitPoints and rounds to the next highest integer
         min = Math.ceil(this.minHitPoints);
+        // Pulls current value in maxHitPoints and rounds down to the next lowest integer
         max = Math.floor(this.maxHitPoints);
+        // Generates a randomized number between the max and min then rounds down to the next lowest integer
         const newHitPoints = Math.floor(Math.random() * (max - min)) + min;
+        // Sets the value of hitPoints to the value randomized by the variable newHitPoints
         this.hitPoints = newHitPoints;
+        // Resets lastDamagetaken back to zero
         this.lastDamageTaken = 0;
+        // Resets isAlive back to true
         this.isAlive = true;
+        // Increases minHitPoints by five after each death to increase difficulty
         this.minHitPoints = this.minHitPoints + 5;
+        // Increases maxHitPoints by five after each death to increase difficulty
         this.maxHitPoints = this.maxHitPoints + 5;
+        // Increases baseDamage by 20% rounded up to the next highest integer
         this.baseDamage = Math.ceil(this.baseDamage * 1.2);
     }
 };
--- player.js
+++ player.js
@@ -4,28 +4,43 @@ const player = {
     hitPoints: 100,
     level: 1,
     baseAttackDamage: 5,
-
+// Everything above this comment are the base stats for player upon loading the page
+// 
+// 
     fight: function(enemy) {
-        rawDamage = enemy.calculateRawDamage();
+        // This pulls the value from any argument with the method of calculateRawDamage
+        const rawDamage = enemy.calculateRawDamage();
+        // This takes the value of the variable rawDamage and subtracts it from the player's current level and replaces the value in the property of lastDamageTaken
         this.lastDamageTaken = rawDamage - this.level;
+        // This variable stores the value of lastDamageTaken subtracted by the player's current hitpoints
         const newHitPoints = this.hitPoints - this.lastDamageTaken;
+        // This takes the value of the player's hitpoints and replaces it with the value of newHitPoints
         this.hitPoints = newHitPoints;
+        // Once the player's hitpoints fall below or to zero the isAlive property becomes false
         if(this.hitPoints <= 0) {
             this.isAlive = false;
         }
+        // Sends the value from the calculatedRawDamage method to an opposing object
         enemy.takeDamage(this.calculateRawDamage());
     },
-
+// This method calculates the raw damage dealt by the player
     calculateRawDamage: function() {
+        // This sets the smallest number possible to one
         const min = Math.min(1);
+        // This calculates a random number using the variable min and adding four to it then rounding up
         const rawDamage = Math.ceil(Math.random() * (min + 4));
+        // This returns the variable rawDamage plus the current value for baseAttackDamage
         return rawDamage + this.baseAttackDamage;
     },
-
+// The following method is applied upon reducing a monsters hitpoints to or below zero
     levelUp: function() {
+        // This takes the player's current level and increases it by 1
         this.level = this.level + 1;
+        // This takes the player's current hit points and heals them for 30 addition points
         this.hitPoints = this.hitPoints + 30;
+        // This resets the lastDamageTaken property back to zero
         this.lastDamageTaken = 0;
+        // This increases the player's base power by 20%
         this.baseAttackDamage = Math.floor(this.baseAttackDamage * 1.2);
     }
 
